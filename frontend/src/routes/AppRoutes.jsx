658e03419ed286909dcbdf2bda91016cb43baa53
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2026-06-10 14:39:01 -0300

    add rotas no AppROutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Cadastro from "../pages/Register/Register";
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home";
+import MentorIA from "../pages/MentorIA/MentorIA";
 import RotaProtegida from "./RotaProtegida";
-
 
 export default function AppRoutes() {
   return (
@@ -11,12 +11,22 @@ export default function AppRoutes() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      
 
       <Route
         path="/home"
         element={
           <RotaProtegida>
             <Home />
+          </RotaProtegida>
+        }
+      />
+
+      <Route
+        path="/mentoria"
+        element={
+          <RotaProtegida>
+            <MentorIA />
           </RotaProtegida>
         }
       />
