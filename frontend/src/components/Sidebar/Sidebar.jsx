3f8b27697e562400278cac4f9895f2c95060f998
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2026-06-10 14:40:15 -0300

    importe de useNavigate no Sidebar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,22 @@
-import logoProjeto from "../../assets/sketch.png"; 
+import { useNavigate } from "react-router-dom";
+import logoProjeto from "../../assets/sketch.png";
 import "./Sidebar.css";
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <aside className="sidebar-container">
       <div className="sidebar-logo">
         <img src={logoProjeto} alt="Logo Mentor IA+" />
       </div>
-      
+
       <ul className="sidebar-menu">
-        <li className="active">Início</li>
+        <li className="active" onClick={() => navigate("/home")}>
+          Início
+        </li>
         <li>Minhas trilhas</li>
-        <li>Mentor IA</li>
+        <li onClick={() => navigate("/mentoria")}>Mentor IA</li>
         <li>Perfil</li>
       </ul>
     </aside>
