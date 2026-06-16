93d34acd4d5d0188a9d36fa09b6c9064084cab2e
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2026-06-16 14:07:21 -0300

    feat: nova tabeça PlanoEstudo

---
+++ src/models/planoEstudo.model.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const PlanoEstudo = sequelize.define('PlanoEstudo', {
+  titulo: { type: DataTypes.STRING, allowNull: false },
+  descricao: { type: DataTypes.TEXT },
+  tempoEstimado: { type: DataTypes.INTEGER, allowNull: false }, // Em horas
+  ordem: { type: DataTypes.INTEGER, allowNull: false },
+  status: {
+    type: DataTypes.ENUM('PENDENTE', 'EM_ANDAMENTO', 'CONCLUIDO'),
+    defaultValue: 'PENDENTE'
+  }
+});
+
+module.exports = PlanoEstudo;
